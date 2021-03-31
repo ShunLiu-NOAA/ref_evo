eddd9b7acbe2bacb6f82581e4f58d7da485bea7d
Shun Liu <Shun.Liu@noaa.gov>
2021-03-31 02:58:05 +0000

    plt_gfs_profl.sh

---
+++ plt_gfs_profl.sh
@@ -0,0 +1,3 @@
+
+python plt_profl.py -i /gpfs/dell2/emc/modeling/noscrub/Shun.Liu/test/ref_evo/20210329/bwi_20210329_00_anl_tm00.csv \
+                    -igus /gpfs/dell2/emc/modeling/noscrub/Shun.Liu/test/ref_evo/20210329/bwi_20210329_00_guess_tm00.csv
