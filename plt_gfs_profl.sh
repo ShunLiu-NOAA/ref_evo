4851aa7e76c325e94f44cfee316727d2ca72eb10
Shun Liu <sliu@Orion-login-3.HPC.MsState.Edu>
2021-04-01 08:19:20 -0500

    plt_gfs_profl.sh

--- plt_gfs_profl.sh
+++ plt_gfs_profl.sh
@@ -1,3 +1,7 @@
 
-python plt_profl.py -i /gpfs/dell2/emc/modeling/noscrub/Shun.Liu/test/ref_evo/20210329/bwi_20210329_00_anl_tm00.csv \
-                    -igus /gpfs/dell2/emc/modeling/noscrub/Shun.Liu/test/ref_evo/20210329/bwi_20210329_00_guess_tm00.csv
+#itile=$1
+# conus domain
+itile=5
+python plt_gfs_profl.py -g /work/noaa/da/Cory.R.Martin/noscrub/UFO_eval/global-workflow/fix/fix_fv3_gmted2010/C768/C768_oro_data.tile${itile}.nc \
+   -c /work/noaa/da/Cory.R.Martin/noscrub/UFO_eval/GFS/para/c768_v15ics_v16tag/2020121500/gdas.20201214/18/atmos/RESTART/20201215.030000.fv_core.res.tile6.nc \
+   -t /work/noaa/da/Cory.R.Martin/noscrub/UFO_eval/GFS/para/c768_v15ics_v16tag/2020121500/gdas.20201214/18/atmos/RESTART/20201215.030000.fv_tracer.res.tile6.nc
